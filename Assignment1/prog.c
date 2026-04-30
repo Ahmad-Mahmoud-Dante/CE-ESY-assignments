513f631e7a2f6250823ce41d107263e951cc5bee
Ahmad-Mahmoud-Dante <gemsstorm12345@gmail.com>
2026-04-30 00:13:26 -0700

    Initial commit for Assignment1

---
+++ Assignment1/prog.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <string.h>
+
+#define SIZE 5
+
+typedef struct {
+    char buffer[SIZE];
+    int head;
+    int tail;
+    int count;
+} CircularBuffer;
+
+void init (CircularBuffer *cb) {
+    cb->head = 0;
+    cb->tail = 0;
+    cb->count = 0;
+}
+
+int isFull (CircularBuffer *cb) {
+    return cb->count == SIZE;
+}
+
+int isEmpty (CircularBuffer *cb) {
+    return cb->count == 0;
+}
+
+void write (CircularBuffer *cb, char data) {
+    if ( isFull(cb) ) {
+        printf("Buffer Overflow\n");
+        return;
+    }
+
+    cb->buffer[cb->tail] = data;
+    cb->tail = (cb->tail + 1) % SIZE;
+    cb->count++;
+}
+
+char read (CircularBuffer *cb) {
+    if (isEmpty(cb)) {
+        printf("Buffer Underflow\n");
+        return '\0';
+    }
+
+    char data = cb->buffer[cb->head];
+    cb->head = (cb->head + 1) % SIZE;
+    cb->count--;
+
+    return data;
+}
+
+int main () {
+    CircularBuffer cb;
+    init(&cb);
+
+    char name[50];
+
+    printf("Enter your name please: ");
+    scanf("%s", name);
+
+    strcat(name, " CE-ESY ");
+
+    printf("Final string: %s\n", name);
+
+    printf("Buffer output: ");
+
+    for (int i = 0; i < strlen(name); i++) {
+
+        
+        if (isFull(&cb)) {
+            printf("%c", read(&cb));
+        }
+
+        write(&cb, name[i]);
+    }
+
+    while (!isEmpty(&cb)) {
+        printf("%c", read(&cb));
+    }
+
+    if (isEmpty(&cb)) {
+        printf("\nBuffer is empty now\n");
+    }
+
+    return 0;
+}
